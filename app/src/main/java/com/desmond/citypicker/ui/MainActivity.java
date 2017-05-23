cf70bb7602df6c399b4fb32f94478c04f1b28c05
yuruizhe <350248823@qq.com>
2017-05-23 12:18:21 +0800

    update

--- app/src/main/java/com/desmond/citypicker/ui/MainActivity.java
+++ app/src/main/java/com/desmond/citypicker/ui/MainActivity.java
@@ -10,13 +10,6 @@ import com.desmond.citypicker.R;
 import com.desmond.citypicker.bean.BaseCity;
 import com.desmond.citypicker.bean.Options;
 import com.desmond.citypicker.finals.KEYS;
-import com.desmond.citypicker.tools.Permissions;
-
-import net.sourceforge.pinyin4j.PinyinHelper;
-import net.sourceforge.pinyin4j.format.HanyuPinyinCaseType;
-import net.sourceforge.pinyin4j.format.HanyuPinyinOutputFormat;
-import net.sourceforge.pinyin4j.format.HanyuPinyinToneType;
-import net.sourceforge.pinyin4j.format.exception.BadHanyuPinyinOutputFormatCombination;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -26,44 +19,6 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new Permissions(this).getSDCard().setOnPermissionsListener(new Permissions.IOnPermissionsListener()
-        {
-            @Override
-            public void callback(boolean arg0)
-            {
-
-            }
-        }).get();
-
-      /*  try
-        {
-            InputStream is = getAssets().open("BaiduMap_cityCode_1102.txt");
-            InputStreamReader isr = new InputStreamReader(is);
-            BufferedReader br = new BufferedReader(isr);
-            String valueString = null;
-
-//            sb.append("insert into tb_city (city_name, city_py, city_py_first, city_code, is_hot) values ");
-
-            while ((valueString = br.readLine()) != null)
-            {
-                String[] s = valueString.split(",");
-                String py = converterToFirstSpell(s[1]);
-                StringBuffer sb = new StringBuffer();
-                sb.append("(");
-                sb.append("'").append(s[1]).append("',");
-                sb.append("'").append(py).append("',");
-                sb.append("'").append(py.substring(0,1)).append("',");
-                sb.append("'").append(s[0]).append("',");
-                sb.append("'").append("F").append("'");
-                sb.append("),");
-                Log.d("!!!!",sb.toString());
-            }
-
-        } catch (IOException e)
-        {
-            e.printStackTrace();
-        }
-*/
 
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener()
         {
@@ -85,25 +40,6 @@ public class MainActivity extends AppCompatActivity
         });
     }
 
-    public static String converterToFirstSpell(String chines)
-    {
-        StringBuffer pinyinName = new StringBuffer();
-        char[] nameChar = chines.toCharArray();
-        HanyuPinyinOutputFormat defaultFormat = new HanyuPinyinOutputFormat();
-        defaultFormat.setCaseType(HanyuPinyinCaseType.UPPERCASE);
-        defaultFormat.setToneType(HanyuPinyinToneType.WITHOUT_TONE);
-        for (int i = 0; i < nameChar.length; i++)
-        {
-            try
-            {
-                pinyinName.append(PinyinHelper.toHanyuPinyinStringArray(nameChar[i], defaultFormat)[0]);
-            } catch (BadHanyuPinyinOutputFormatCombination e)
-            {
-                e.printStackTrace();
-            }
-        }
-        return pinyinName.toString();
-    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data)
